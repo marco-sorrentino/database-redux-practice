8966b8ed0451e308aeb98b122cf0e5044a4ab8ef
Marco-Sorrentino <sorrentmarco@gmail.com>
2023-02-22 19:38:15 +0100

    splitting reducer

---
+++ src/components/redux/reducer/userReducer.js
@@ -0,0 +1,19 @@
+import { GO_TO_DETAIL } from "../action";
+
+const initialState = {
+  users: [],
+};
+
+const mainReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case GO_TO_DETAIL:
+      return {
+        ...state,
+        users: [action.payload],
+      };
+    default:
+      return state;
+  }
+};
+
+export default mainReducer;
